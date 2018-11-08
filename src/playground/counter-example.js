ce7171c7c4ad0c1f682df5ddd1d2fbd4a1197ca7
Ercan Er <iosyazilim@gmail.com>
2018-11-08 15:15:41 +0300

    Add ComponentDidMount

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -10,11 +10,28 @@ class Counter extends React.Component {
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
+        try{
+			const stringCount = localStorage.getItem("count");
+			const count = parseInt(stringCount, 10);
+	
+			if(!isNaN(count)){
+				this.setState(() => ({ count }));
+				
+			}
+		} catch(e){
 
+        }
+        
+		console.log("Fetching Data(Tr:Verileri getirmek için)");
+    
     }
-    componentDidUpdate(prevProps, prevState){
 
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.count !== this.state.count){
+			localStorage.setItem("count",this.state.count);
+			console.log("Saving Data (Tr: Verileri Kaydetmek için)");
+		}
     }
     componentWillUnmount(){
 
@@ -58,36 +75,3 @@ class Counter extends React.Component {
 
 const yaz = document.getElementById('app');
 ReactDOM.render(<Counter />, yaz);
-/* State kullanmadan yapılan counter app */
-// let count = 0;
-// const addOne = () => {
-//     count += 1;
-//     renderCounterApp();
-   
-// };
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// };
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-// };
-// const someId = "myIdHere";
-
-// const appRoot = document.getElementById("app");
-
-// const renderCounterApp = () => {
-//     const templateThree = (
-//         <div>
-//             <h1>Count: {count}</h1>
-//             <button id={someId} className="button" onClick={addOne}>+1</button> {/*class="" bu sekilde değil className ="" seklinde tanımlanır */}
-//             <button onClick={minusOne}>-1</button>
-//             <button onClick={reset}>Reset</button>
-//         </div>
-        
-//         );
-       
-//         ReactDOM.render(templateThree, appRoot);
-// };
-//     renderCounterApp();
