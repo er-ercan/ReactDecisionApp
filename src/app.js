b07bec3da8eaf1b657412b1237367da7b183ec4c
Ercan Er <iosyazilim@gmail.com>
2018-11-14 14:10:44 +0300

    React React-Dom

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-// import './utils'
+import validator from 'validator';
 
-import anythingIwant, { square, add, ercan } from './utils';
-import minus, { isAdult, isDrink } from './person';
-console.log("app.js is running");
-console.log(square(5));
-console.log(add(4,6));
-console.log(ercan);
-isAdult(20);
-isDrink(33);
-console.log(anythingIwant(100, 35));
-console.log(minus(100, 5, 6));
+const valid = validator.isEmail('ercanatilim@.com');
+console.log(valid);
+
+
+
+//import anythingIwant, { square, add, ercan } from './utils';
+//import minus, { isAdult, isDrink } from './person';
+//console.log("app.js is running");
+//console.log(square(5));
+//console.log(add(4,6));
+//console.log(ercan);
+//isAdult(20);
+//isDrink(33);
+//console.log(anythingIwant(100, 35));
+//console.log(minus(100, 5, 6));
