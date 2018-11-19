dbab92a5181f3d07cd84725a70341ad5742e54d3
Ercan Er <iosyazilim@gmail.com>
2018-11-19 15:49:33 +0300

    Add normalize.css
    I added normalize css because all browser optimized.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecesionApp';
-import './styles/style.scss'
+import './styles/style.scss';
+import 'normalize.css/normalize.css'
 import validator from 'validator';
 
 ReactDOM.render(
