f3a8626b5d4b188144c164f89e916943cf6b11eb
Ercan Er <iosyazilim@gmail.com>
2018-11-19 17:19:05 +0300

    React Sass Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Header = (props) => (
-	<div className='header'>
+	<div className='header'>	
+		<div className='container'>
 		<h1 className='hearder__title'>{props.title}</h1>
 		{props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
 		<h3>{props.sub}</h3>
+		</div>
 	</div>
 );
 
