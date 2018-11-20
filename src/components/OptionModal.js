1c81ba8e22ca0c9dd46de91059c6ceb13c6e55c9
Ercan Er <iosyazilim@gmail.com>
2018-11-20 10:15:08 +0300

    React Style Update

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,15 +3,18 @@ import Modal from 'react-modal';
 
 const OptionModal = (props) => (
         <Modal
+            className='modal'
             isOpen={!!props.selectedOption}
             contentLabel="Selected Label"
             onRequestClose={props.closeModal}
-            ariaHideApp={false}>
-            <h3>
+            ariaHideApp={false}
+            closeTimeoutMS={200}
+            >
+            <h3 className='modal__title'>
                 Selected Option
             </h3>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
-            <button onClick={props.closeModal}>Okay</button>
+            {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p>}
+            <button className='button' onClick={props.closeModal}>Okay</button>
         </Modal>
     );
 
